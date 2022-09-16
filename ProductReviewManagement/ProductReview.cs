406ba73c45dadd2830c0162b5ced640ed6a808ae
praveenmethraskar <praveenmethraskar@gmail.com>
2022-09-16 21:11:10 +0530

    Product review

---
+++ ProductReviewManagement/ProductReview.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class ProductReview
+    {
+        public int ProductId { get; set; }
+
+        public int UserId { get; set; }
+
+        public int rating { get; set; }
+        public string review { get; set; }
+        public bool islike { get; set; }    
+    }
+}
---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace ProductReviewManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Product review management");
+            List<ProductReview> productreviewlist = new List<ProductReview>()
+            {
+                new ProductReview(){ProductId = 1, UserId = 1,rating=5,review="good",islike=true},
+                new ProductReview(){ProductId = 2, UserId = 1,rating=2,review="bad",islike=true},
+                new ProductReview(){ProductId = 3, UserId = 1,rating=5,review="good",islike=true},
+                new ProductReview(){ProductId = 4, UserId = 1,rating=4,review="bad",islike=true},
+                new ProductReview(){ProductId = 5, UserId = 1,rating=3,review="good",islike=true}
+            };
+
+            foreach(var products in productreviewlist)
+            {
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine(products.ProductId+" | "+products.UserId+" | "+products.rating+" | "+products.review+" | "+products.islike);
+
+            }
+        }
+    }
+}
